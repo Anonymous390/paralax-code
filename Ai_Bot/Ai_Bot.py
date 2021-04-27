c00457e02a4935b83e884fdbabb247dab827125f
Ray Chen <tankimarshal2@gmail.com>
2021-04-27 16:59:29 +0100

    Started data reading

---
+++ Ai_Bot/Ai_Bot.py
@@ -0,0 +1,29 @@
+def message():
+    msg = ""
+    msg = input()
+    tokens = msg.lower().split()
+    words = [e.lower() for e in tokens]
+    if len(words[0]) == 1:
+        msg = ''.join(words)
+    if len(words[0]) >= 2:
+        msg = ' '.join(words)
+
+    # General Conversation
+    if (msg == "hi" ) or (msg == "hello"):
+        print("Heyyy how are you?")
+    if msg == "im fine how are you":
+        print("Thats great to hear! Im fine as well!")
+    
+    # Political Questions
+
+    # India
+    if "president of india" in msg:
+        print("Ram Nath Kovind is the Current President of India")
+    if "prime minister of india" in msg:
+        print("Narendra Modi Is the current Prime Minister of India")
+    if msg == "Who is the P":
+        print('nothing')
+
+while True: 
+    message()
+
