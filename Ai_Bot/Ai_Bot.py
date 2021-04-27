dae6f3890585b7e938e6cc3cf5959f67dcaecca5
Ray Chen <tankimarshal2@gmail.com>
2021-04-27 17:33:41 +0100

    removed hard-coded qs

--- Ai_Bot/Ai_Bot.py
+++ Ai_Bot/Ai_Bot.py
@@ -9,7 +9,7 @@ for file in os.listdir("data"):
     is_data.extend({item[1]:item[2] for item in filter(lambda x: x[0] == "is", data)})
     in_data.extend({item[1]:item[2] for item in filter(lambda x: x[0] == "in", data)})
 
-def message2():
+def message():
     msg = input("> ")
     tokens = msg.lower().split()
     words = [e.lower() for e in tokens]
@@ -25,32 +25,6 @@ def message2():
             if key in msg:
                 print(in_data[key])
 
-def message():
-    msg = ""
-    msg = input()
-    tokens = msg.lower().split()
-    words = [e.lower() for e in tokens]
-    if len(words[0]) == 1:
-        msg = ''.join(words)
-    if len(words[0]) >= 2:
-        msg = ' '.join(words)
-
-    # General Conversation
-    if (msg == "hi" ) or (msg == "hello"):
-        print("Heyyy how are you?")
-    if msg == "im fine how are you":
-        print("Thats great to hear! Im fine as well!")
-    
-    # Political Questions
-
-    # India
-    if "president of india" in msg:
-        print("Ram Nath Kovind is the Current President of India")
-    if "prime minister of india" in msg:
-        print("Narendra Modi Is the current Prime Minister of India")
-    if msg == "Who is the P":
-        print('nothing')
-
 while True: 
-    message2()
+    message()
 
