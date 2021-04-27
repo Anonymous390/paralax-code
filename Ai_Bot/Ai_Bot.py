9448bfe6234c55da8c45f71ff2e6db025b9d1adc
Ray Chen <tankimarshal2@gmail.com>
2021-04-27 17:26:58 +0100

    Multiple data files

--- Ai_Bot/Ai_Bot.py
+++ Ai_Bot/Ai_Bot.py
@@ -1,10 +1,16 @@
-with open("data.txt") as f:
-    data = [item.split(",") for item in f.readlines()]
-is_data = {item[1]:item[2] for item in filter(lambda x: x[0] == "is", data)}
-in_data = {item[1]:item[2] for item in filter(lambda x: x[0] == "in", data)}
+import os
+
+is_data = {}
+in_data = {}
+
+for file in os.listdir("data"):
+    with open(file) as f:
+        data = [item.rstrip().split(",") for item in f.readlines()]
+    is_data.extend({item[1]:item[2] for item in filter(lambda x: x[0] == "is", data)})
+    in_data.extend({item[1]:item[2] for item in filter(lambda x: x[0] == "in", data)})
 
 def message2():
-    msg = input()
+    msg = input("> ")
     tokens = msg.lower().split()
     words = [e.lower() for e in tokens]
     if len(words[0]) == 1:
