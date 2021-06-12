b305596ddcdb97f1a897b020385b4cc047c34b62
Ray Chen <tankimarshal2@gmail.com>
2021-06-12 14:33:39 +0100

    Commands bot

--- discord-bot/main.py
+++ discord-bot/main.py
@@ -1,23 +1,23 @@
 import discord
+from discord.ext import commands
 import os
 import dotenv
 
 dotenv.load_dotenv()
 token = os.getenv("TOKEN")
 
-client = discord.Client()
+client = commands.Bot(command_prefix="#")
 
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    
-    if message.content.startswith('#hello'):
-        await message.channel.send('Hello!')
+@client.command(aliases=["h"])
+async def hello(ctx):
+    await ctx.send("Hello!")
 
+@client.command(aliases=["c"])
+async def cookie(ctx):
+    await ctx.send("Have a ")
 
 client.run(token)
