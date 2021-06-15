196cb26d231591e4255514df4bcb1b0f18392082
Anonymous390 <achyutha.nr10@gmail.com>
2021-06-15 21:46:51 +0530

    Added moderation commands

--- discord-bot/img_commands.py
+++ discord-bot/img_commands.py
@@ -1,14 +1,34 @@
 # Discord.py imports
+from PIL import Image, ImageEnhance, ImageFilter, ImageOps
+import cv2
+import os
+import numpy as np
+import random
+import cv2
 import discord
-from PIL import Image, ImageOps
 from discord.ext import commands
 from io import BytesIO
 from init import client
-import os
 
-bot = commands.Bot(command_prefix='!')
+def sp_noise(image,prob):
+    '''
+    Add salt and pepper noise to image
+    prob: Probability of the noise
+    '''
+    output = np.zeros(image.shape,np.uint8)
+    thres = 1 - prob
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            rdn = random.random()
+            if rdn < prob:
+                output[i][j] = 0
+            elif rdn > thres:
+                output[i][j] = 255
+            else:
+                output[i][j] = image[i][j]
+    return output
 
-@bot.event
+@client.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.errors.CheckFailure):
         await ctx.send('You do not have the correct role for this command.')
@@ -49,3 +69,42 @@ async def triggered(ctx, member: discord.Member = None):
 
     await ctx.send(file = discord.File("output.png"))
     os.remove("output.png")
+
+@client.command()
+async def sparkle(ctx, memeber: discord.Member = None):
+    if memeber == None:
+        member = ctx.author
+    foregorund = Image.open("sparkles.png")
+    asset = member.avatar_url_as(size =128)
+    data = BytesIO(await asset.read())
+    background = Image.open(data)
+    resized_background = background.resize((500, 500))
+    resized_foreground = foregorund.resize((500, 500))
+
+    resized_background.paste(resized_foreground, (0, 0), resized_foreground.convert("RGBA"))
+    resized_background.save("output.png")
+
+    await ctx.send(file = discord.File("output.png"))
+    os.remove("output.png")
+
+@client.command()
+async def deepfry(ctx, member: discord.Member = None):
+    if member == None:
+        member = ctx.author
+
+    im = Image.open("images.jpeg")
+
+    enhancer = ImageEnhance.Brightness(im)
+
+    im = enhancer.enhance(0.5)
+    im = im.filter(ImageFilter.GaussianBlur(radius = 1.4))
+    converter = ImageEnhance.Color(im)
+    im = converter.enhance(10)
+    im.save('rev.png')
+    image = cv2.imread('rev.png')
+    noise_img = sp_noise(image,0.05)
+    cv2.imwrite('output.png', noise_img)
+
+    await ctx.send(file = discord.File("output.png"))
+    os.remove("output.png")
+    os.remove("rev.png")
---
+++ discord-bot/moderation_commands.py
@@ -0,0 +1,64 @@
+from discord.ext import commands
+import discord
+from init import client
+
+@client.event()
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.CheckFailure):
+        await ctx.send('You do not have the correct role for this command.')
+
+@client.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send("Looks like you don't have the permissions and the required role.")
+
+@client.command()
+@client.has_role(853334263968301118)
+async def kick(ctx, member: discord.Member, *, reason=None):
+    await member.kick(reason=reason)
+    await ctx.send(f'User {member.mention} has been kicked!')
+
+@client.command()
+@client.has_role(853334263968301118)
+async def ban(ctx, member: discord.Member, *, reason=None):
+    await member.ban(reason=reason)
+    await ctx.send(f'User {member.mention} has been banned!')
+
+@client.command()
+@client.has_role(853334263968301118)
+async def mute(ctx, member: discord.Member):
+    muted_role = discord.utils.get(ctx.guild.roles, name="Muted")
+    verified_role = discord.utils.get(ctx.guild.roles, name="verified")
+    await member.add_roles(muted_role)
+    await member.remove_roles(verified_role)
+    await ctx.send(f'User {member.mention} has been muted!')
+
+@client.command()
+@client.has_role(853334263968301118)
+async def unmute(ctx, member: discord.Member):
+    muted_role = discord.utils.get(ctx.guild.roles, name="Muted")
+    verified_role = discord.utils.get(ctx.guild.roles, name="verified")
+    await member.add_roles(verified_role)
+    await member.remove_roles(muted_role)
+    await ctx.send(f'User {member.mention} has been unmuted!')
+
+@client.command()
+@client.has_role(853334263968301118)
+async def unban(ctx, *, member):
+    banned_users = await ctx.guild.bans()
+    member_name, member_discriminator = member.split("#")
+
+    for ban_entry in banned_users:
+        user = ban_entry.user
+        if(user.name, user.discriminator) == (member_name, member_discriminator):
+            await ctx.guild.unban(user)
+            await ctx.send(f'User {user.name}#{user.discriminator} has been unbanned!')
+            return
+
+@client.command()
+@client.has_role(853334263968301118)
+async def warn(ctx, member: discord.Member, *, reason):
+    if member.id in [ctx.author.id, client.user.id]:
+        return await ctx.send("You cant warn yourself or the bot!")
+
+    await ctx.send(f'{member.mention} you are being warned!')
